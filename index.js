57e7f955c819d2179a47d2650c315b7eaf6b53e8
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2024-09-05 19:38:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
 #! /usr/bin/env node
 
 console.log(`
-╭──────────────────────────────────────────────╮
-│                                              │
-│  Hey ⊂(◉‿◉)つ                                │
-│  My name is Pouya Saadeghi and I'm a         │
-│  freelance web developer, living in Istanbul │
-│                                              │
-├──────────────────────────────────────────────┤
-│                                              │
-│  - Currently I'm working on the popular,     │
-│    open-source UI library, daisyUI:          │
-│    https://daisyui.com                       │
-│                                              │
-│  - My open-source projects and experiments   │
-│    are available on GitHub:                  │
-│    https://github.com/saadeghi               │
-│                                              │
-├──────────────────────────────────────────────┤
-│                                              │
-│  📋 Resume:    https://saadeghi.com/resume   │
-│  💼 Portfolio: https://saadeghi.com/projects │
-│  🐥 X:         https://x.com/saadeghi        │
-│  📫 Email:     pouya.saadeghi@gmail.com      │
-│                                              │
-╰──────────────────────────────────────────────╯
+╭───────────────────────────────╮
+│  Hey!                         │
+│  My name is Pouya Saadeghi    │
+│  I'm a web developer,         │
+│  living in Istanbul           │
+├───────────────────────────────┤
+│ - Currently I'm working on    │
+│   daisyUI which is a popular  │
+│   open-source UI library:     │
+│   https://daisyui.com         │
+│ - My open-source projects     │
+│   are available on GitHub:    │
+│   https://github.com/saadegh  │
+├───────────────────────────────┤
+│  https://saadeghi.com         │
+│  https://x.com/saadeghi       │
+│  pouya.saadeghi@gmail.com     │
+╰───────────────────────────────╯
 `);
